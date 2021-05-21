635899a5471e8009c0a7dbdb556bd3c5efe6364a
letsgo247 <letsgo247@hanmail.net>
2021-05-21 23:30:33 +0900

    delete pycache

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -16,14 +16,27 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+import os, environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env(
+    env_file= os.path.join(BASE_DIR, '.env')
+)
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)fb-89^vj_%066=xi^-k++)3q3=9%-nk82v8$+p6k=#r(t@xck'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
